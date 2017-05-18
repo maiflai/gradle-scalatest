bdfddc4f09c1a2ffab03b47048b87b96d60aaca2
Sheridan C Rawlins <scr@yahoo-inc.com>
2017-05-18 09:07:29 -0700

    Ditch spock - just use junit directly.

--- src/test/groovy/com/github/maiflai/ScalaTestTaskPluginFunctionalTest.groovy
+++ src/test/groovy/com/github/maiflai/ScalaTestTaskPluginFunctionalTest.groovy
@@ -1,36 +1,33 @@
 package com.github.maiflai
 
-import spock.lang.Specification
+import org.junit.Test
+
+import static org.hamcrest.CoreMatchers.is
+import static org.hamcrest.MatcherAssert.assertThat
 
 /**
  * @author scr on 5/17/17.
  */
-class ScalaTestTaskPluginFunctionalTest extends Specification {
-    def "Plugin should run both testng & scalatest tests"() {
-        given:
+class ScalaTestTaskPluginFunctionalTest {
+    @Test
+    void testTestngAndScalatest() {
         File projectDir = new File('src/test/examples/testngAndScalatest')
         def launcher = SetupBuild.setupBuild(projectDir)
-
-        when:
         launcher.forTasks('clean', 'check').run()
-        and:
+
         File testResultsDir = new File(projectDir, 'build/test-results')
         File scalatestsResultsDir = new File(testResultsDir, 'scalatest')
         File scalatestTestFile = new File(scalatestsResultsDir, 'TEST-ScalatestTest.xml')
         File testngResultsDir = new File(testResultsDir, 'test')
         File testngTestFile = new File(testngResultsDir, 'TEST-TestngTest.xml')
-        then:
-        scalatestTestFile.exists()
-        testngTestFile.exists()
 
-        when:
+        assertThat(scalatestTestFile.path + " Doesn't exist", scalatestTestFile.exists())
+        assertThat(testngTestFile.path + " Doesn't exist", testngTestFile.exists())
+
         def scalatestTestResults = new XmlSlurper().parse(scalatestTestFile)
-        then:
-        scalatestTestResults.testcase.@name.text() == 'scalatest test should pass'
+        assertThat(scalatestTestResults.testcase.@name.text(), is('scalatest test should pass'))
 
-        when:
         def testngTestResults = new XmlSlurper().parse(testngTestFile)
-        then:
-        testngTestResults.testcase.@name.text() == 'testPass'
+        assertThat(testngTestResults.testcase.@name.text(), is('testPass'))
     }
 }
