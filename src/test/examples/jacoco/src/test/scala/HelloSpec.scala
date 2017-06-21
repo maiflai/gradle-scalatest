1acd193a148f54bff97363c8948df28c9672da07
Stu <maiflai@users.noreply.github.com>
2017-06-21 06:47:06 +0100

    verity that main and test resources are available on the classpath

--- src/test/examples/jacoco/src/test/scala/HelloSpec.scala
+++ src/test/examples/jacoco/src/test/scala/HelloSpec.scala
@@ -4,4 +4,10 @@ class HelloSpec extends FunSuite {
   test("it should cover") {
     assert(new Hello().say() === "world")
   }
+  test("main resource") {
+    assert(Option(getClass.getResourceAsStream("/main.txt")).isDefined)
+  }
+  test("test resource") {
+    assert(Option(getClass.getResourceAsStream("/test.txt")).isDefined)
+  }
 }
